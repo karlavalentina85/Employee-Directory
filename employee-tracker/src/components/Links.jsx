655023c619b21c091f064bc72be9bc34d5009b7c
Blackbird <ksilva.rod@gmail.com>
2020-09-22 22:00:58 -0500

    react components

---
+++ employee-tracker/src/components/Links.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Collapse = styled.div.attrs({
+    className: 'collpase navbar-collapse',
+})``
+
+const List = styled.div.attrs({
+    className: 'navbar-nav mr-auto',
+})``
+
+const Item = styled.div.attrs({
+    className: 'collpase navbar-collapse',
+})``
+
+class Links extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Link to="/" className="navbar-brand">
+                    My Employee Tracker
+                </Link>
+                <Collapse>
+                    <List>
+                        <Item>
+                            <Link to="/employees/list" className="nav-link">
+                                List Employees
+                            </Link>
+                        </Item>
+                        <Item>
+                            <Link to="/employees/create" className="nav-link">
+                                Create Employee
+                            </Link>
+                        </Item>
+                    </List>
+                </Collapse>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Links
