a97dc90646849d03a07d54ab36ad341e2497787f
Blackbird <ksilva.rod@gmail.com>
2020-09-23 01:48:31 -0500

    Updates

---
+++ precache-manifest.03ca1aa0bf08354020fc3c757eeb8f4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdcd2fe2e36c07a54f367e067c74a0c8",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "7de37f4fb9ed8b008812",
+    "url": "/Employee-Directory/static/css/2.e2e9731f.chunk.css"
+  },
+  {
+    "revision": "7de37f4fb9ed8b008812",
+    "url": "/Employee-Directory/static/js/2.5c154569.chunk.js"
+  },
+  {
+    "revision": "0c8eeaa5da09482dd4b9",
+    "url": "/Employee-Directory/static/js/main.60410140.chunk.js"
+  },
+  {
+    "revision": "1794aedb7da2e07d4918",
+    "url": "/Employee-Directory/static/js/runtime~main.51f3d348.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Employee-Directory/static/media/logo.5d5d9eef.svg"
+  }
+]);
